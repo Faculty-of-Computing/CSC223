c005db5fe79846990b0e893414dfd7e3d5885543
Gideonjon <gideonjones63@gmail.com>
2025-06-30 14:47:29 +0100

    json-file-added-and-new-index

--- script.js
+++ script.js
@@ -1,325 +1,53 @@
-var quiz = {
-  user: "Dave",
-  questions: [
-    {
-      text: "A term that refers to the way in which the nodes of a network are linmked together is called?",
-      responses: [
-        { text: "Interconnectivity" },
-        { text: "Topology", correct: true },
-        { text: "Connection" },
-        { text: "Network" }] },
-        
-        {
-          text: "If you are display text which cannot be modified by the user, use the?",
-          responses: [
-          { text: "Lable control", correct: true },
-          { text: "TextBox control" },
-          { text: "Name control" },
-          { text: "Caption control" }] },
+let app;
 
-          
-  {
-    text: "The Pseudocode is another name for?",
-    responses: [
-    { text: "Object code" },
-    { text: "Algorithm", correct: true },
-    { text: "Flowchart" },
-    { text: "Coding" }] },
-    
+fetch("data/csc223_all_questions.json")
+  .then((res) => res.json())
+  .then((quiz) => {
+    const userResponseSkeleton = Array(quiz.questions.length).fill(null);
 
-  {
-    text: "In Visual Basic, the decleration of variables is done by_____key word?",
-    responses: [
-    { text: "dim", correct: true },
-    { text: "int" },
-    { text: "declare" },
-    { text: "static" }] },
-
-
-    
-    {
-      text: "What is flowchart?",
-      responses: [
-        { text: "Is a solution in the program" },
-        { text: "It is a pictorial structure of a program", correct: true },
-        { text: "it is drawing of a structure" },
-        { text: "It is a step by step solution of a program" }] },
-        
-        {
-          text: "Which of the following is not a keyword?",
-          responses: [
-          { text: "Read" },
-          { text: "start", correct: true },
-          { text: "Write" },
-          { text: "endif" }] },
-
-  {
-    text: "When the user clicks a button,_______ is triggered?",
-    responses: [
-    { text: "an event", correct: true },
-    { text: "a setting" },
-    {
-      text: "an order" },
-
-    { text: "a property" }] },
-
-
-  {
-    text: "In Visual Basic, which of the following is a valid name for a varible?",
-    responses: [
-    { text: "$two_One" },
-    {
-      text: "Two One" },
-
-    { text: "Two_One", correct: true },
-    { text: "2_One" }] },
-
-
-  {
-    text:
-    "The rectangle shape in the flowcahrt diagram is used for what?",
-    responses: [
-    { text: "Decision" },
-    { text: "Start/Stop" },
-    { text: "Processing", correct: true },
-    { text: "Logic fow" }] },
-
-
-  {
-    text: "What is text box used for in the Visual Basic environment? ",
-    responses: [
-    { text: "Used for design" },
-    {text: "Used for dragging text" },   
-    { text: "Used to accept user input", correct: true },
-    { text: "used to display a text" }] },
-
-
-    {
-      text: "Finding and fixing programming errors is called?",
-      responses: [
-      { text: "recording" },
-      { text: "tuning" },
-      { text: "debugging", correct: true },
-      { text: "fix-its" }] },
-
-  {
-    text: "The CancelButton is a property that belongs to____",
-    responses: [
-    { text: "Option" },
-    { text: "Label", correct: true },
-    { text: "TextBox" },
-    { text: "Button" }] },
-
-
-  {
-    text:
-    "The diamond shape in a flowchart stands for?",
-    responses: [
-    {text: "Input" },
-    { text: "Process" },
-    { text: "Decision", correct: true },
-    { text: "Connectors" }] },
-
-    {
-      text: "Which data type is best for 'City name'",
-      responses: [
-      { text: "String", correct: true },
-      { text: "Integer" },
-      { text: "Floating Point" },
-      { text: "Boolean" }] },
-    
-  {
-    text: "The following phrase are involved in programming execpt?",
-    responses: [
-    { text: "Writing the program" },
-    { text: "Modelling the program", correct: true },
-    { text: "Devising an algroithm" },
-    { text: "Problem definition " }] },
-
-
-  {
-    text: "The dirctional arrow shape in the flowchart is used for?",
-    responses: [
-    { text: "Logic flow", correct: true },
-    { text: "Processing" },
-    { text: "Decision" },
-    { text: "Start/Stop" }] },
-
-
-  {
-    text: "The trapozodial shape in thw flowchart is used for?",
-    responses: [
-    { text: "Starting/Stopping" },
-    { text: "Subroutine" },
-    { text: "Input/Output", correct: true },
-    { text: "Decision/Processing" }] },
-
-
-  {
-    text: "What is the correct statment when declearing and assigning the value of 100 to an integer variable called numPeople?",
-    responses: [
-    { text: " Dim numPeople = 100" },
-    { text: "Dim NumPeople As Integer = 100" },
-    { text: "Dim numPeople As Integer = 100", correct: true },
-    { text: "Dim numpeople As Integer = 100" }] },
-
-
-  {
-    text: "What is the code to close a form?",
-    responses: [
-    { text: "Form.Exit()" },
-    { text: "Me.Close()", correct: true },
-    { text: "ME.Clear()" },
-    { text: "Form.Clear()" }] },
-
-
-  {
-    text: "In Visual Basic we work with objects, which have?",
-    responses: [
-    { text: "Projects, Solutions, and Procedures" },
-    { text: "Classes, Actions, and Disciplines" },
-    { text: "Properties, Methods, and Events", correct: true },
-    { text: "Programmer preference" }] },
-
-
-  {
-    text: "What is the purpose of the ToolBox?",
-    responses: [
-    { text: "To design user-defined methods" },
-    { text: "To select methods to be placed on the form" },
-    { text: "To select controls with assiocited even procedures"},
-    { text: "To select controls and place on an apllication form", correct: true }]},
-
-
-  {
-    text: "How would you get the Design window to display if it is not visible in your project?",
-    responses: [
-    { text: "Click View on the menu bar, and then on the View menu click Designer", correct: true },
-    { text: " Press Shift+f9 on the keyboard" },
-    { text: "Select view designer in the edit menu" },
-    { text: "Double-click the icon in the toolbar for the designer window" }] },
-
-
-  {
-    text: "Which is considered a valid step for developing a Visual Basic application?",
-    responses: [
-    { text: "Create a flowchart or pseudocode version of each method" },
-    { text: "Make a list of the controls needed" },
-    { text: "Make a list of the methods needed for each control"},
-    { text: "All of the above", correct: true}] },
-
-
-
-
-  {
-    text: "Which of the following is NOT a Visual Basic Control?",
-    responses: [
-    { text: "TextBox" },
-    { text: "Label" },
-    { text: "Algorithm", correct: true },
-    { text: "Form" }] },
-
-
-  {
-    text: "When creating a VB application, you will initially,",
-    responses: [
-    { text: "Check for logic errors" },
-    { text: "Correct syntax errors" },
-    { text: "Write program statements"},
-    { text: "Change control properties", correct:true }] },
-
-
-  {
-    text: "When your program is in run time mode, you ____",
-    responses: [
-    { text: "Can create the user interface " },
-    { text: "Can always edit your program" },
-    { text: "Can interact with the program as it executes", correct: true },
-    { text: "Can save your program" }] },
-
-
-
-
-  {
-    text: "When a student is learning computer programming, it is traditional to start by learning to write ____program.",
-    responses: [
-    { text: "Psuedocode" },
-    { text: "Simple" },
-    { text: "Hello World", correct: true },
-    { text: "Algorithm" }] },
-
-
-  {
-    text: "The default property of a textbox control is .......",
-    responses: [
-    { text: "Text" },
-    { text: "Name" },
-    { text: "Enable"},
-    { text: "Visible", correct: true}] },
-
-
-  {
-    text: "What loop control is best suited when you want to repeat a group of statements a specified number of times and loop index counts the number of the loop iterations as the loop executes?",
-    responses: [
-    { text: "Do" },
-    { text: "For Each .. Next" },
-    { text: "For Next", correct: true },
-    { text: "While" }] },
-
-
-  {
-    text: "A file that contains a program code is called a ....file?",
-    responses: [
-    { text: "C#" },
-    { text: "CS" },
-    { text: "Program"},
-    { text: "Sourse code", correct: true }] },
-  ] },
-
-
-
-
-userResponseSkelaton = Array(quiz.questions.length).fill(null);
-
-var app = new Vue({
-  el: "#app",
-  data: {
-    quiz: quiz,
-    questions: quiz.questions,
-    questionIndex: 0,
-    userResponses: userResponseSkelaton,
-    quizStarted: true,
-    isActive: false },
-
-  filters: {
-    charIndex: function (i) {
-      return String.fromCharCode(97 + i);
-    } },
-
-
-  methods: {
-    selectOption: function (index) {
-      Vue.set(this.userResponses, this.questionIndex, index);
-    },
-    next: function () {
-      if (this.questionIndex < this.quiz.questions.length)
-      this.questionIndex++;
-    },
-
-    prev: function () {
-      if (this.quiz.questions.length > 0) this.questionIndex--;
-    },
-    score: function () {
-      var score = 0;
-      for (let i = 0; i < this.userResponses.length; i++) {
-        if (
-        typeof this.quiz.questions[i].responses[
-        this.userResponses[i]] !==
-        "undefined" &&
-        this.quiz.questions[i].responses[this.userResponses[i]].correct)
-        {
-          score = score + 1;
-        }
-      }
-      return score;
-    } } });
+    app = new Vue({
+      el: "#app",
+      data: {
+        quiz,
+        questions: quiz.questions,
+        questionIndex: 0,
+        userResponses: userResponseSkeleton,
+        quizStarted: false,
+        showForm: false,
+      },
+      filters: {
+        charIndex: function (i) {
+          return String.fromCharCode(65 + i); // A, B, C...
+        },
+      },
+      methods: {
+        selectOption(index) {
+          Vue.set(this.userResponses, this.questionIndex, index);
+        },
+        next() {
+          if (this.questionIndex < this.quiz.questions.length) {
+            this.questionIndex++;
+          }
+        },
+        prev() {
+          if (this.questionIndex > 0) {
+            this.questionIndex--;
+          }
+        },
+        score() {
+          let score = 0;
+          for (let i = 0; i < this.userResponses.length; i++) {
+            const question = this.quiz.questions[i];
+            const selectedIndex = this.userResponses[i];
+            if (question.responses[selectedIndex] && question.responses[selectedIndex].correct) {
+              score++;
+            }
+          }
+          return score;
+        },
+      },
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to load quiz JSON:", err);
+  });
